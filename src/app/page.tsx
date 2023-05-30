292b31e0384af3c6aeba4441d88b82fce762f117
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2023-05-29 22:33:48 -0300

    Projeto em desenvolvimento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ import {Search} from "./Search";
 
 export default function Home() {
   return (
+    
    <main>
+    <title>Web app previsão de tempo</title>
       <Search
       placeholder="Digite o nome de uma Cidade"
       />
